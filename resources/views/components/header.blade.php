7fbedf77768ff494a8faebd3aaa929e29074a5e5
Alansan <alaansannchezz@gmail.com>
2022-09-30 16:09:43 -0300

    Se añadio Ruta y vista para Index y estilos

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg text-success">
+    <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center w-100">
+            <div>
+                <a href="#" class="navbar-brand text-success fw-bold">Evertec System</a>
+            </div>
+            <nav>
+                <a href="#" class="nav-link">Home</a>
+            </nav>
+        </div>
+
+    </div>
+</nav>
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layout')
+
+@section('titulo', 'Shop')
+
+@section('content')
+
+    <h1 class="text-center">Bienvenido a nuestra Tienda</h1>
+
+@endsection
