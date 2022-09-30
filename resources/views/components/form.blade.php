a8cda29175bd6763636feddc76902b207eda2f46
Alansan <alaansannchezz@gmail.com>
2022-09-30 16:25:09 -0300

    Añadiendo formulario (Component)

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,16 @@
+<form class="w-50 mx-auto mt-4">
+    @csrf
+    <div class="mb-3">
+        <label for="name" class="form-label">Ingresa tu Nombre</label>
+        <input type="text" id="name" class="form-control" name="customer_name">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Ingresa tu Correo</label>
+        <input type="email" id="email" class="form-control" name="customer_email">
+    </div>
+    <div class="mb-3">
+        <label for="phone" class="form-label">Ingresa tu Telefono</label>
+        <input type="number" id="phone" class="form-control" name="customer_mobile">
+    </div>
+    <button type="submit" class="btn btn-success">Comprar</button>
+</form>
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -6,5 +6,6 @@
 
     <h1 class="text-center mt-4">Estás a punto de comprar <span class="fw-bold text-success text-capitalize">{{ $product->product_name }}</span></h1>
     <h3 class="text-center">Sigue las instrucciones</h3>
+    @include('components.form')
 
 @endsection
