1953a44600ab8239a6bbfcfe3a7591003995e67c
Alansan <alaansannchezz@gmail.com>
2022-09-30 16:16:34 -0300

    Creando Shop Model y Migracion. Factory para Products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Product::class;
+    
     public function definition()
     {
         return [
-            //
+            'product_name' => $this->faker->word(),
+            'product_price' => $this->faker->randomFloat(null, 50, 9999)
         ];
     }
 }
